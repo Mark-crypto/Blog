fbd280dcd393d8dd518362326acd1b708d794ac4
Mark-crypto <Mark.Onyango@strathmore.edu>
2025-02-22 12:40:51 +0300

    building frontend

--- components/Mission.jsx
+++ components/Mission.jsx
@@ -4,8 +4,15 @@ const Mission = () => {
   return (
     <>
       <div>
-        <div className="mission">Mission</div>
-        <div className="vision">Vision</div>
+        <div className="mission">
+          All of the biggest technological inventions created by man - the
+          airplane, the automobile, the computer - says little about his
+          intelligence, but speaks volumes about his laziness.
+        </div>
+        <div className="vision">
+          Just because something doesn’t do what you planned it to do doesn’t
+          mean it’s useless
+        </div>
       </div>
     </>
   );
